388078edbe1e5dea666d4a3026a1d822a21f5e7a
TomVerschueren <tom.bassman@hotmail.com>
2017-10-12 11:15:52 +0200

    opdracht-verbeter + map opdracht comments

---
+++ opdracht-verbeter/opdracht-verbeter.php
@@ -0,0 +1,20 @@
+<?php
+	$tekst 			= 	'Test 123...';
+	$two_tekst 		= 	'Test 456';
+	$_tekst 		= 	'test';
+	$zin 			= 	'Jean D\'Haeseleir gaat naar Praag!!';
+	$beschrijving 	= 	'Vogelbekdier';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>opdracht-verbeter</title>
+</head>
+<body>
+
+	<h1>Foutjes verbeteren</h1>
+
+</body>
+</html>
