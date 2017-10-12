3d329496e2ccfe214f8be9b92c51a273c74c4beb
TomVerschueren <tom.bassman@hotmail.com>
2017-10-12 10:56:34 +0200

    opdracht-comments

---
+++ opdracht-comments.php
@@ -0,0 +1,15 @@
+<?php
+$voornaam = "Tom";
+$naam = "Verschueren";
+# Tom Verschueren tom.verschueren@student.kdg.be
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>opdracht-comments</title>
+</head>
+<body>
+    <p><?= $voornaam ?> <?= $naam ?></p>
+</body>
+</html>
